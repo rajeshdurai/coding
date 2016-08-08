ad8999763e9002633c919d1b03b11f537c098048
rajeshdurai <drajesh13196@gmail.com>
2016-08-07 22:21:55 -0700

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,10 @@
+public class Palindrome {
+public static void main(String[]args)
+	    {
+	    
+				StringBuffer s=new StringBuffer(args[0]);
+	    	s.reverse();
+	    System.out.println(s);
+	    }
+}
+	    
