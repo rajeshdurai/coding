e394832d643cb9f8abc32c77178f689c07a9e92a
rajeshdurai <drajesh13196@gmail.com>
2016-08-07 22:09:18 -0700

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,9 @@
+public class Reverse {
+public static void main(String[] args) {
+	String a="Ajithkumar";
+	StringBuffer b=new StringBuffer();
+	b.append(a);
+	b.reverse();
+	System.out.println(b);
+	}
+}
